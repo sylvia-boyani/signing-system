1b88a33992df52336fe762130fb125e8c35d2902
Tiberius Mairura <tiberiusmairura90@gmail.com>
2022-05-19 18:03:15 +0300

    implement student sign in

--- assets/js/student.js
+++ assets/js/student.js
@@ -1,7 +1,42 @@
-function myFunction(){
-    document.getElementById("inOut").innerHTML = "successfully checked in"
-}
+const studentIdInput = document.querySelector(".student-id");
 
-function myFunction2(){
-    document.getElementById("inOut").innerHTML = "successfully checked out"
-}
+const buttons = document.querySelectorAll(".controls");
+
+const isValidId = (id) => {
+  let validId = /[0-9]/;
+  return validId.test(id) ? true : false;
+};
+
+const checkinStudent = (id) => {
+  fetch(`http://localhost:8080/api/v1/students/${id}`)
+  .then(response=>response.json())
+  .then(data=>{
+      console.log(data)
+  })
+};
+
+const checkoutStudent = (id) => {
+    fetch(`http://localhost:8080/api/v1/students/${id}`)
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data)
+    })
+  };
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let value = parseInt(studentIdInput.value);
+    if (isValidId(value)) {
+        studentIdInput.classList.add("border-success")
+        studentIdInput.classList.remove("border-warning")     
+      if (button.classList.contains("check-in")) {
+        checkinStudent(value);
+      } else {
+        checkoutStudent(value);
+      }
+    } else {
+        studentIdInput.classList.add("border-warning")
+        studentIdInput.classList.remove("border-success")
+    }
+  });
+});
